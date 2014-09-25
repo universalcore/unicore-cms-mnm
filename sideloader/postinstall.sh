6b2bd21b2e9b62644a633d666a257647e205acc6
Milton Madanda <madandat@gmail.com>
2014-09-25 12:01:33 +0200

    ensure pyramid app is installed with -e

--- sideloader/postinstall.sh
+++ sideloader/postinstall.sh
@@ -1,4 +1,5 @@
 pip="${VENV}/bin/pip"
 cd /var/praekelt/unicore-cms-ffl/ && echo `pwd` && ./install_libgit2 && cd ~/
 
-$pip install cffi praekelt-python-gitmodel /var/praekelt/unicore-cms-ffl/
+$pip install cffi praekelt-python-gitmodel
+$pip install -e /var/praekelt/unicore-cms-ffl/
