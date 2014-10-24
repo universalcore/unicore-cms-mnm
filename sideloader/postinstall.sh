9e5605cbc2c41da548f99f6f58e657a9bacd8f70
Simon de Haan <simon@praekeltfoundation.org>
2014-10-24 15:27:04 +0200

    re-sync post deploy

--- sideloader/postinstall.sh
+++ sideloader/postinstall.sh
@@ -1,2 +1,13 @@
 pip="${VENV}/bin/pip"
-$pip install -e /var/praekelt/unicore-cms-ffl/
+
+cd "/var/${INSTALLDIR}/${REPO}/"
+
+$pip install -e .
+
+ini_files="ffl.*.ini"
+
+for ini in $ini_files
+do
+    `which eg-tools` resync -c $ini -m unicore.content.models.Category
+    `which eg-tools` resync -c $ini -m unicore.content.models.Page
+done
