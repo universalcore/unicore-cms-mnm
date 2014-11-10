0863b0af86c62053b9509ec3517292872e5a1c2d
Simon de Haan <simon@praekeltfoundation.org>
2014-11-11 00:32:14 +0200

    add -r

--- sideloader/postinstall.sh
+++ sideloader/postinstall.sh
@@ -8,6 +8,6 @@ ini_files="ffl.*.ini"
 
 for ini in $ini_files
 do
-    `which eg-tools` resync -c $ini -m unicore.content.models.Category -f mappings/category.mapping.json
-    `which eg-tools` resync -c $ini -m unicore.content.models.Page -f mappings/page.mapping.json
+    `which eg-tools` resync -c $ini -m unicore.content.models.Category -f mappings/category.mapping.json -r true
+    `which eg-tools` resync -c $ini -m unicore.content.models.Page -f mappings/page.mapping.json -r true
 done
