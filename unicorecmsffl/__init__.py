07cc35221d47fdc3776057468c62772b24a53b19
Milton Madanda <madandat@gmail.com>
2014-11-18 13:24:38 +0200

    remove credits page

--- unicorecmsffl/__init__.py
+++ unicorecmsffl/__init__.py
@@ -6,14 +6,9 @@ def main(global_config, **settings):
     """
     config = Configurator(settings=settings)
     config.add_translation_dirs('unicorecmsffl:locale')
-    config.include('unicorecmsffl')
     config.include('cms')
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.scan()
 
     config.override_asset('cms:templates/', 'unicorecmsffl:templates/')
     return config.make_wsgi_app()
-
-
-def includeme(config):
-    config.add_route('credits', '/credits/')
